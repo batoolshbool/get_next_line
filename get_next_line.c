f64fbead5f1daf74d4521ea4361a65a9a879c8f6
batoolshbool <batoolmoe201@gmail.com>
2025-09-19 13:18:34 +0300

    Sep 19 - 13:18

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bshbool <bshbool@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 10:03:28 by bshbool           #+#    #+#             */
-/*   Updated: 2025/09/19 13:08:29 by bshbool          ###   ########.fr       */
+/*   Updated: 2025/09/19 13:17:42 by bshbool          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 char	*get_next_line(int fd)
 {
 	ssize_t	fileread;
+	size_t	BUFFER_SIZE;
 	char	buffer[BUFFER_SIZE];
 
 	fileread = read(fd, buffer, BUFFER_SIZE);
