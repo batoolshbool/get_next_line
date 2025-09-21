010db7b32dce93214074c871ea283589c67481cf
batoolshbool <batoolmoe201@gmail.com>
2025-09-21 18:42:59 +0300

    Sep 21 - 18:42

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bshbool <bshbool@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 10:03:28 by bshbool           #+#    #+#             */
-/*   Updated: 2025/09/19 13:17:42 by bshbool          ###   ########.fr       */
+/*   Updated: 2025/09/21 18:15:51 by bshbool          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,23 @@
 
 char	*get_next_line(int fd)
 {
-	ssize_t	fileread;
-	size_t	BUFFER_SIZE;
+	int		fileread;
 	char	buffer[BUFFER_SIZE];
+	int 	i = 0;
 
-	fileread = read(fd, buffer, BUFFER_SIZE);
-	if (fileread < 0)
+	 char *hi = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	 fileread = read(fd, buffer, BUFFER_SIZE);
+	 if (fd < 0 || BUFFER_SIZE <= 0 || !hi)
 		return (NULL);
+	 while (fileread > 0)
+	 {
+		hi[i] = fileread;
+		if (fileread == '\0' || fileread == '\n')
+			return(hi);
+		i++;
+	 }
+	 if (i == 0)
+	 	return (NULL);
+	hi[i] = '\0';
+	 return(hi);
 }
